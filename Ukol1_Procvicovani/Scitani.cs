7273bc1064b5490d2ad2452aaf6e7432097815e4
Mikey20TR <Mikejbc@email.cz>
2024-04-24 23:02:21 +0200

    Scitani

---
+++ Ukol1_Procvicovani/Scitani.cs
@@ -0,0 +1,16 @@
+﻿namespace Ukol1_Procvicovani
+{
+    internal class Scitani
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, uživateli, mohu ti pomoci sečíst dvě zadaná čísla");
+            Console.WriteLine("=======================================================");
+            Console.WriteLine("Zadej první číslo:");
+            int cislo1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej druhé číslo:");
+            int cislo2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Součet čísel {cislo1} a {cislo2} je {cislo1 + cislo2}");
+        }
+    }
+}
