fd184ea8d844eca1d06b6dc4892682b9a80fab60
Mikey20TR <Mikejbc@email.cz>
2024-05-28 21:10:49 +0200

    Graficke Objekty

---
+++ Lekce7_GrafickeObjekty/GrafickyObjekt.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekce7_GrafickeObjekty
+{
+    public class GrafickyObjekt
+    {
+        public int Vyska;
+        public int Sirka;
+        public string Barva;
+
+        public virtual void Vykreslit()
+        {
+            Console.WriteLine("X");
+        }
+        public void Vybarvi()
+        {
+            switch(Barva)
+            {
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "white":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+            }
+        }
+    }
+    
+    public class Obdelnik : GrafickyObjekt
+    {
+        public Obdelnik(int sirka, int vyska, string barva)
+        {
+            Sirka = sirka;
+            Vyska = vyska;
+            Barva = barva;
+        }
+        public override void Vykreslit()
+        {
+            Vybarvi();
+            for (int cisloRadku = 1; cisloRadku <= Vyska; cisloRadku++)
+            {
+                for (int i = 0; i < Sirka; i++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine("");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("");
+        }
+    }
+    public class Trojuhelnik : GrafickyObjekt
+    {
+        public Trojuhelnik(int vyska, string barva)
+        {
+            Vyska = vyska;
+            Barva = barva;
+        }
+        public override void Vykreslit()
+        {
+            Vybarvi();
+            for (int cisloRadku = 1; cisloRadku <= Vyska; cisloRadku++)
+            {
+                for (int i = 1; i <= cisloRadku; i++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine("");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("");
+        }
+    }
+    public class Text : GrafickyObjekt
+    {
+        public string _Text;
+        public Text(string text, string barva)
+        {
+            _Text = text;
+            Barva = barva;
+        }
+            
+        public override void Vykreslit()
+        {
+            Vybarvi();
+            Console.WriteLine(_Text);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
---
+++ Lekce7_GrafickeObjekty/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+
+namespace Lekce7_GrafickeObjekty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GrafickyObjekt o1 = new Obdelnik(12, 4, "blue");
+            GrafickyObjekt t1 = new Trojuhelnik(8, "yellow");
+            GrafickyObjekt text1 = new Text("Ahoj, Czechitas", "green");
+
+            var list = new List<GrafickyObjekt> { o1, t1, text1 };
+            foreach (GrafickyObjekt objekt in list)
+            {
+                objekt.Vykreslit();
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Zadej šířku obdélníku:");
+            int sirkaO2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej výšku obdélníku:");
+            int vyskaO2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nyní si můžeš vybrat barvu obdélníku (Red/Blue/Green/White/Yellow):");
+            string barvaO2 = Console.ReadLine().ToLower();
+            GrafickyObjekt o2 = new Obdelnik(sirkaO2, vyskaO2, barvaO2);
+
+            o2.Vykreslit();
+
+            Console.WriteLine("Zadej výšku trojúhelníku:");
+            int vyskaT2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nyní si můžeš vybrat barvu trojúhelníku (Red/Blue/Green/White/Yellow):");
+            GrafickyObjekt t2 = new Trojuhelnik(vyskaT2, Console.ReadLine().ToLower());
+
+            t2.Vykreslit();
+
+            Console.WriteLine("Zadej libovolný text:");
+            string textUzivatele = Console.ReadLine();
+            Console.WriteLine("A nyní si můžeš vybrat barvu svého textu (Red/Blue/Green/White/Yellow):");
+            GrafickyObjekt text2 = new Text(textUzivatele,Console.ReadLine().ToLower());
+
+            var list2 = new List<GrafickyObjekt> { o2, t2, text2 };
+            foreach (GrafickyObjekt objekt in list2)
+            {
+                objekt.Vykreslit();
+            }
+        }
+    }
+}
